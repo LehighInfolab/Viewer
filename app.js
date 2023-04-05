7e7b1c46efbb884104fa72dbccc61cfd8043e4ea
Ala Chua <anc224@lehigh.edu>
2023-04-05 15:54:05 -0400

    app.js updated

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+var router = express.Router()
 
 /* need spawn for running command line arguments on the server */
 const { spawn } = require('child_process');
@@ -13,35 +12,9 @@ const { spawn } = require('child_process');
 /* use multer for file uploads */
 const multer = require('multer');
 
-/* multer definitions
- * these define storage and upload functions to be used for multer
- * aren't necessary in app.js since they have been moved to index.js
- * currently commented out to make sure the routes all work without them here
-*/
-/*
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname)
-  }
-})
+require('dotenv').config()
 
-const upload = multer({
-  storage: storage,
-  // ensure that file uploaded is a .SURF file 
-  fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(SURF)$/)) {
-      return cb(new Error('Input must be a .SURF file'))
-    }
-    cb(undefined, true)
-  }
-})
-*/
-//const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
-var indexRouter = require('./routes/index');
 var app = express();
 
 // view engine setup
@@ -56,8 +29,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/tree-scripts', express.static(path.join(__dirname, '/node_modules/smart-webcomponents-community/source/modules/')))
 app.use('/tree-styles', express.static(path.join(__dirname, '/node_modules/smart-webcomponents-community/source/styles/')))
 
+
 /* send the app to the correct router based on the URL */
+var indexRouter = require('./routes/index');
 app.use('/', indexRouter); //all routes are in the index router, found in index.js
+
+const uploadsRouter = require('./routes/uploads')
+app.use('/uploads', uploadsRouter)
+
+const executablesRouter = require('./routes/executables')
+app.use('/executables', executablesRouter)
+
 
 /* catch 404 and forward to error handler */
 app.use(function (req, res, next) {
@@ -76,4 +58,16 @@ app.use(function (err, req, res, next) {
 });
 
 
-module.exports = app;
+/* set up mongodb to connect to database 
+Mongodb has not been set up yet. Commented out for now until we need to use it.
+*/
+// const mongoose = require('mongoose');
+// const uri = process.env.MONGODB_URI
+
+// mongoose.connect(uri, { useNewUrlParser: true })
+// const db = mongoose.connection
+// db.on('error', (error) => console.error(error))
+// db.once('open', () => console.log('Connected to database'))
+
+
+module.exports = app, this.router;
